a3e7e20d928f82ea176b414e31e0f352d0a5d5e9
Bradley Chatha <bradley@chatha.dev>
2024-12-03 19:26:04 +0000

    critical: how dare VSCode use tabs in D code

--- day2/source/app.d
+++ day2/source/app.d
@@ -1,80 +1,80 @@
 struct Report
 {
-	int[] levels;
-	bool isSafe;
-	bool isSortaSafe;
+    int[] levels;
+    bool isSafe;
+    bool isSortaSafe;
 }
 
 void main(string[] args)
 {
-	import std.algorithm : map, splitter, count;
-	import std.array     : array;
-	import std.conv      : to;
-	import std.file      : readText;
-	import std.math      : abs;
-	import std.stdio     : writeln;
-	import std.string    : lineSplitter;
+    import std.algorithm : map, splitter, count;
+    import std.array     : array;
+    import std.conv      : to;
+    import std.file      : readText;
+    import std.math      : abs;
+    import std.stdio     : writeln;
+    import std.string    : lineSplitter;
 
-	const input = readText(args[1]);
-	auto reports = 
-		input
-		.lineSplitter()
-		.map!((line) => Report(line.splitter(' ').map!(to!int).array))
-		.array;
+    const input = readText(args[1]);
+    auto reports = 
+        input
+        .lineSplitter()
+        .map!((line) => Report(line.splitter(' ').map!(to!int).array))
+        .array;
 
-	// Find safety for part 1
-	NextReport: foreach(ref report; reports)
-	{
-		bool shouldDescend = report.levels[0] > report.levels[1];
-		foreach(i, level; report.levels)
-		{
-			if(i == 0)
-				continue;
+    // Find safety for part 1
+    NextReport: foreach(ref report; reports)
+    {
+        bool shouldDescend = report.levels[0] > report.levels[1];
+        foreach(i, level; report.levels)
+        {
+            if(i == 0)
+                continue;
 
-			const diff = report.levels[i-1] - report.levels[i];
-			const descends = diff > 0;
-			if((!descends && shouldDescend) || (descends && !shouldDescend))
-				continue NextReport;
-			else if(diff.abs < 1 || diff.abs > 3)
-				continue NextReport;
-		}
-		report.isSafe = true;
-	}
+            const diff = report.levels[i-1] - report.levels[i];
+            const descends = diff > 0;
+            if((!descends && shouldDescend) || (descends && !shouldDescend))
+                continue NextReport;
+            else if(diff.abs < 1 || diff.abs > 3)
+                continue NextReport;
+        }
+        report.isSafe = true;
+    }
 
-	// Find safety for part 2
-	// Almost certainly a better way to do this, but it works, so meh.
-	// One idea is to recursively call findSaftey only when we identify bad numbers,
-	// rather than trying to ignore all of them.
-	void findSafety(ref Report report, size_t ignoreIndex)
-	{
-		bool shouldDescend = (ignoreIndex == 0)
-			? report.levels[1] > report.levels[2]
-			: (ignoreIndex == 1)
-				? report.levels[0] > report.levels[2]
-				: report.levels[0] > report.levels[1];
-		foreach(i, level; report.levels)
-		{
-			const curr = (i == ignoreIndex) ? i - 1 : i;
-			const next = (i == ignoreIndex - 1) ? i + 2: i + 1;
+    // Find safety for part 2
+    // Almost certainly a better way to do this, but it works, so meh.
+    // One idea is to recursively call findSaftey only when we identify bad numbers,
+    // rather than trying to ignore all of them.
+    void findSafety(ref Report report, size_t ignoreIndex)
+    {
+        bool shouldDescend = (ignoreIndex == 0)
+            ? report.levels[1] > report.levels[2]
+            : (ignoreIndex == 1)
+                ? report.levels[0] > report.levels[2]
+                : report.levels[0] > report.levels[1];
+        foreach(i, level; report.levels)
+        {
+            const curr = (i == ignoreIndex) ? i - 1 : i;
+            const next = (i == ignoreIndex - 1) ? i + 2: i + 1;
 
-			if(next >= report.levels.length || curr >= report.levels.length)
-				continue;
+            if(next >= report.levels.length || curr >= report.levels.length)
+                continue;
 
-			const diff 					= report.levels[curr] - report.levels[next];
-			const descends 				= diff > 0;
-			const isExpectedDirection 	= descends == shouldDescend;
-			const isInBounds 			= diff.abs >= 1 && diff.abs <= 3;
-			if(!isExpectedDirection || !isInBounds)
-				return;
-		}
-		report.isSortaSafe = true;
-	}
-	foreach(ref report; reports)
-	{
-		foreach(i; 0..report.levels.length)
-			findSafety(report, i);
-	}
+            const diff 					= report.levels[curr] - report.levels[next];
+            const descends 				= diff > 0;
+            const isExpectedDirection 	= descends == shouldDescend;
+            const isInBounds 			= diff.abs >= 1 && diff.abs <= 3;
+            if(!isExpectedDirection || !isInBounds)
+                return;
+        }
+        report.isSortaSafe = true;
+    }
+    foreach(ref report; reports)
+    {
+        foreach(i; 0..report.levels.length)
+            findSafety(report, i);
+    }
 
-	writeln("Part 1: ", reports.count!"a.isSafe"());
-	writeln("Part 2: ", reports.count!"a.isSortaSafe"());
+    writeln("Part 1: ", reports.count!"a.isSafe"());
+    writeln("Part 2: ", reports.count!"a.isSortaSafe"());
 }
